251b846bf657345cb413a16f290953fe7264f849
Cameron Buscher <cameronbuscher@gmail.com>
2018-05-31 09:32:02 -0600

    Initial commit, mid-redux setup

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
 export const fakeAction = () => ({ type: 'FAKE'})
+
+export const getPokeTypes = pokemonInfo => ({
+  type: 'GET_POKE_TYPES',
+  types: pokemonInfo
+})
+
+export const isLoading = boolean => ({
+  type: 'IS_LOADING',
+  isLoading: boolean
+})
+
+export const hasErrored = boolean => ({
+  type: 'HAS_ERRORED',
+  hasErrored: boolean
+})
+
+ex
