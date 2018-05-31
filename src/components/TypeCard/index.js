c9e4d000215e86ec4494654108a2bf6d4687ccd3
Cameron Buscher <cameronbuscher@gmail.com>
2018-05-31 10:42:20 -0600

    Fetch pokemon of corresponding type when card is clicked, store them in state

--- src/components/TypeCard/index.js
+++ src/components/TypeCard/index.js
@@ -9,10 +9,24 @@ export class TypeCard extends Component {
       pokemon: []
     }
   }
+
+  fetchPokemon = () => {
+    if (!this.state.pokemon.length) {
+      this.props.pokemon.map(pokemONE => {
+        return fetch(`http://localhost:3001/pokemon/${pokemONE}`)
+          .then(response => response.json())
+          .then(data => {
+            this.setState({
+              pokemon: [...this.state.pokemon, data]
+            })
+          })
+      })
+    }
+  }
   
   render() {
     return (
-      <div className='typeCard'>
+      <div className='typeCard' onClick={this.fetchPokemon}>
         <h3>{this.props.name}</h3>
       </div>
     )
