f6f782a32eb25def24dbd37237ffdd7ac8b935f2
Cameron Buscher <cameronbuscher@gmail.com>
2018-05-31 09:47:33 -0600

    Add thunk function for getting pokemon types

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,5 +18,16 @@ export const hasErrored = boolean => ({
 export const fetchPokemonTypes = url => {
   return dispatch => {
     dispatch(isLoading(true))
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          dispatch(hasErrored(true))
+          throw Error('Oh dang')
+        }
+        dispatch(isLoading(false))
+        return response
+      })
+      .then(response => response.json())
+      .then(pokeData => dispatch(getPokeTypes(pokeData)))
   }
 }
